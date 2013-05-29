2b88bf701f1ddf3a710bbdc5a03fac04e425501a
Steve <stevemargc@gmail.com>
2013-05-29 17:55:10 -0400

    added a helper to display the quantity available on the item show page

---
+++ app/helpers/items_helper.rb
@@ -0,0 +1,11 @@
+module ItemsHelper
+  def format_quantity(quantity)
+    if quantity == 0
+      "So sorry, this item is currently sold out!"
+    elsif quantity == 1
+      "Only 1 available"
+    else
+      "#{quantity} available."
+    end
+  end
+end
