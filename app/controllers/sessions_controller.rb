5e7a2e54cbb17ee6b953126dca141967e6915bbd
Steve <stevemargc@gmail.com>
2013-06-13 15:19:38 -0400

    Added flash message for login, logout and invalid login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
+      flash[:notice] = "Welcome back to Home!"
       redirect_back_or root_url
     else
-      render 'new', notice: "Invalid email/password combination"
+      flash.now.alert = "Email or password is invalid"
+      render 'new'
     end
   end
   
   def destroy
     sign_out
+    flash[:notice] = "You have successfully logged out"
     redirect_to root_url
   end
   
