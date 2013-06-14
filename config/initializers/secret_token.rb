f4365e655548b01b471a9762d6e0a74c8dc45cba
Steve <stevemargc@gmail.com>
2013-06-14 17:27:51 -0400

    generated new secret token and moved to env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Home::Application.config.secret_token = 'd2bd90cb2eebcc9b427194dd563c940ad7909c922a1a0804b8f9bc38c88c4fc9a971b15a1216963e782f79400d48f661efa07de95a517bae98f2158e6c83c595'
+Home::Application.config.secret_token = ENV["SECRET_TOKEN"]
