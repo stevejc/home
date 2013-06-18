a1df728f937c5e03d9afc56725e0676911cf5f74
Steve <stevemargc@gmail.com>
2013-06-18 19:46:09 -0400

    added the ability to edit and delete shipping addresses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,14 @@ module ApplicationHelper
     end
   end
   
+  
+  def display_address(address)
+    content_tag(:p, content_tag(:b, address.name)) +
+    content_tag(:p, address.address1) +
+    content_tag(:p, address.address2) +
+    content_tag(:p, address.city + ", " + address.state + " " + address.zip)
+  end
+  
+
 end
 
