e1735464c43ceefe9e4237f4d6abc1fa15309c4f
Steve <stevemargc@gmail.com>
2013-06-28 15:22:17 -0400

    moved item status to item helper and added styling to emphasize status

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -28,4 +28,31 @@ module ItemsHelper
       "Your item is not available for sale because the quantity available is zero. If you still have items available please update the quantity and change the status to Available."
     end
   end
+  
+  def display_item_status(item) 
+    cc = case item.status
+    when "Pending" 
+      "alert-error"
+    when "Available"
+      "alert-success"
+    else 
+      "alert-info"
+    end 
+    
+    content_tag(:div, 
+    (content_tag(:h3, "Status: #{item.status}") + 
+      if item.status == "Pending"
+        content_tag(:p, "Your item is not yet listed for sale on HomePlace. Every item listed for sales requires at least one image, if your item is ready to go update the status to Available.") +
+        link_to("/items", class: "btn") do
+        "list for sale"
+        end
+      elsif item.status == "Available"
+        content_tag(:p,"Your item is listed for sale on HomePlace. If your item is no longer for sale please update the status to pending or change the quantity available to zero.")
+      else
+        content_tag(:p, "Your item is not available for sale because the quantity available is zero. If you still have items available please update the quantity and change the status to Available.")
+      end    
+    ), id:"item-status", class: "#{cc}")
+
+  end  
+  
 end
