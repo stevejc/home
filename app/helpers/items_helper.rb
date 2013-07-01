4097107ae5bc7cecac6493d54db38711cd4f004c
Steve <stevemargc@gmail.com>
2013-07-01 17:23:11 -0400

    removed old helper method display_status_update, no longer needed

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -19,16 +19,6 @@ module ItemsHelper
     end
   end
   
-  def display_status_update(item)
-    if item.status == "Available"
-      "Your item is listed for sale on HomePlace. If your item is no longer for sale please update the status to pending or change the quantity available to zero."
-    elsif item.status == "Pending"
-      "Your item is not yet listed for sale on HomePlace. Every item listed for sales requires at least one image, if your item is ready to go update the status to Available."
-    else
-      "Your item is not available for sale because the quantity available is zero. If you still have items available please update the quantity and change the status to Available."
-    end
-  end
-  
   def display_item_status(item) 
     cc = case item.status
     when "Pending" 
@@ -42,8 +32,11 @@ module ItemsHelper
     content_tag(:div, 
     (content_tag(:h3, "Status: #{item.status}") + 
       if item.status == "Pending"
-        content_tag(:p, "Your item is not yet listed for sale on HomePlace. Every item listed for sales requires at least one image, if your item is ready to go update the status to Available.") + (content_tag(:i, " ", class: "icon-ok") + content_tag(:h5, "Add at least 1 image!") if item.itemimages.empty?) + (content_tag(:i, " ", class: "icon-ok") + content_tag(:h5, "Set-up your shop to accept credit cards with Stripe?", class: "inline") if !item.shop.stripe_shop_token?) +
-
+        content_tag(:p, "Your item is not yet listed for sale on HomePlace. Every item listed for sales requires at least one image, if your item is ready to go update the status to Available.") + 
+        (content_tag(:i, " ", class: "icon-ok") + 
+        content_tag(:h5, "Add at least 1 image!") if item.itemimages.empty?) + 
+        (content_tag(:i, " ", class: "icon-ok") + 
+        content_tag(:h5, "Set-up your shop to accept credit cards with Stripe?", class: "inline") if !item.shop.stripe_shop_token?) +
         (if item.itemimages.exists? && item.shop.stripe_shop_token?
           link_to("/items", class: "btn") do
             "list for sale"
@@ -55,7 +48,6 @@ module ItemsHelper
         content_tag(:p, "Your item is not available for sale because the quantity available is zero. If you still have items available please update the quantity and change the status to Available.")
       end    
     ), id:"item-status", class: "#{cc}")
-
   end  
   
 end
